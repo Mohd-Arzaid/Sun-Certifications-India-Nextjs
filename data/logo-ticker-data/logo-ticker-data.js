90fc222b79c518315c83baccf72d8f6cb49e1a0f
Abhay raj kashyap <abhayrajkashyap2002@gmail.com>
2026-06-29 12:14:25 +0530

    Add LogoTicker component and logo data for client logos

---
+++ data/logo-ticker-data/logo-ticker-data.js
@@ -0,0 +1,57 @@
+const honeywellLogo = "/client-logos/honeywell.svg";
+const ikeaLogo = "/client-logos/ikea.svg";
+const panasonicLogo = "/client-logos/panasonic.svg";
+const ansellLogo = "/client-logos/ansell.svg";
+const ametekLogo = "/client-logos/ametek.svg";
+const boschLogo = "/client-logos/bosch.svg";
+
+export const COMPANY_LOGOS = [
+  {
+    name: "Honeywell",
+    src: honeywellLogo,
+    height: 37,
+    width: 160,
+    alt: "Honeywell Company Logo - Trusted Partner",
+    title: "Honeywell Company Logo - Trusted Partner",
+  },
+  {
+    name: "IKEA",
+    src: ikeaLogo,
+    height: 30,
+    width: 160,
+    alt: "IKEA Company Logo - Trusted Partner",
+    title: "IKEA Company Logo - Trusted Partner",
+  },
+  {
+    name: "Panasonic",
+    src: panasonicLogo,
+    height: 26,
+    width: 160,
+    alt: "Panasonic Company Logo - Trusted Partner",
+    title: "Panasonic Company Logo - Trusted Partner",
+  },
+  {
+    name: "Ansell",
+    src: ansellLogo,
+    height: 26,
+    width: 160,
+    alt: "Ansell Company Logo - Trusted Partner",
+    title: "Ansell Company Logo - Trusted Partner",
+  },
+  {
+    name: "Ametek",
+    src: ametekLogo,
+    height: 23.5,
+    width: 160,
+    alt: "Ametek Company Logo - Trusted Partner",
+    title: "Ametek Company Logo - Trusted Partner",
+  },
+  {
+    name: "Bosch",
+    src: boschLogo,
+    height: 32,
+    width: 160,
+    alt: "Bosch Company Logo - Trusted Partner",
+    title: "Bosch Company Logo - Trusted Partner",
+  },
+];
