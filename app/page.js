fe4241555c09f97b9c5b153391a9c9d3d627608c
Abhay raj kashyap <abhayrajkashyap2002@gmail.com>
2026-06-29 12:45:31 +0530

    added audits marquee ih the home page

--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 import Hero from "@/components/manual/home-page-sections/Hero";
 import LogoTicker from "@/components/manual/home-page-sections/LogoTicker";
 import OurServices from "@/components/manual/home-page-sections/OurServices";
+import AuditsMarquee from "@/components/manual/home-page-sections/AuditsMarquee";
 
 const Home = () => {
   return (
@@ -8,6 +9,7 @@ const Home = () => {
       <Hero />
       <LogoTicker />
       <OurServices />
+      <AuditsMarquee />
     </>
   );
 };
